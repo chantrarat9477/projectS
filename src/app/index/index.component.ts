dbe3c0ad0108ffa084e954351038236b54a29346
chantrarat9477 <chantrarat.si.58@ubu.ac.th>
2018-04-28 17:42:00 +0700

    ส่งกราฟ

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -12,19 +12,25 @@ export class IndexComponent implements OnInit {
   ngOnInit() {
   }
 
-  //โค้ดที่เพิ่มมา -->
-
-  // lineChart
-  public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+  
+  // ข้อมูลในเส้นกราฟมี24ข้อมูลตามเวลา24ชั่วโมง
+  public lineChartData: Array<any> = [
+    { data: [27.7, 30, 50, 80, 5, 33, 99, 22, 89, 9, 22, 89, 27.7, 30, 50, 80, 5, 33, 99, 22, 89, 9, 22, 89], label: 'อุณหภูมิ' },
+    { data: [20, 80, 34, 42, 19, 17, 20, 22, 89, 9, 22, 89, 67.5, 40.3, 50, 80, 5, 33, 99, 22, 89, 9, 22, 89], label: 'ความชื้น' }
+ 
   ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions:any = {
+
+  //อาเรย์ที่เก็บข้อมูลในแต่ละชั่วโมง
+  public lineChartLabels: Array<any> = 
+  ['00:00','01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00','09:00','10:00','11:00',
+   '12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00'];
+
+
+  public lineChartOptions: any = {
     responsive: true
   };
-  public lineChartColors:Array<any> = [
+
+  public lineChartColors: Array<any> = [
     { // grey
       backgroundColor: 'rgba(148,159,177,0.2)',
       borderColor: 'rgba(148,159,177,1)',
@@ -41,37 +47,31 @@ export class IndexComponent implements OnInit {
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgba(77,83,96,1)'
     },
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    }
   ];
-  public lineChartLegend:boolean = true;
-  public lineChartType:string = 'line';
- 
-  public randomize():void {
-    let _lineChartData:Array<any> = new Array(this.lineChartData.length);
+  public lineChartLegend: boolean = true;
+  public lineChartType: string = 'line';
+
+  //ไม่แน่ใจว่าเก็บไว้ทำไมแต่อยากให้เก็บไว้
+  public randomize(): void {
+    let _lineChartData: Array<any> = new Array(this.lineChartData.length);
     for (let i = 0; i < this.lineChartData.length; i++) {
-      _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
+      _lineChartData[i] = { data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label };
       for (let j = 0; j < this.lineChartData[i].data.length; j++) {
         _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
       }
     }
     this.lineChartData = _lineChartData;
   }
- 
+  //ไม่แน่ใจว่าเก็บไว้ทำไมแต่อยากให้เก็บไว้
+
   // events
-  public chartClicked(e:any):void {
+  public chartClicked(e: any): void {
     console.log(e);
   }
- 
-  public chartHovered(e:any):void {
+
+  public chartHovered(e: any): void {
     console.log(e);
   }
-  //โค้ดที่เพิ่มมา -->
+  
 
 }
